27ebe8aefaafb2843089f1f908075a186a773847
BugHunter-X <askzenai@gmail.com>
2024-12-31 08:24:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when you try to access a property of an object that is null or undefined.  It's often seen when dealing with asynchronous operations where data might not be available immediately.  For example, if you try to render a component before the data it needs is fetched.
